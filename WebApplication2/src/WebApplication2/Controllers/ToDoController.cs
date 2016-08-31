daac0a45ea0cdd93b5d514143e33b222d1f54a23
David Myers <DavidMyers@gamestop.com>
2016-08-31 17:35:49 -0500

    check in

---
+++ WebApplication2/src/WebApplication2/Controllers/ToDoController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+using System.Data.Common;
+using System.Data;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        public ToDoController(IToDoRepository todoItems)
+        {
+
+            DBTest test = new DBTest();
+            string ctn = test.GetCarton();
+
+            ToDoItems = todoItems;
+        }
+        public IToDoRepository ToDoItems { get; set; }
+
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return ToDoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetToDo")]
+        public IActionResult GetById(string id)
+        {
+            var item = ToDoItems.Find(id);
+            if (TempData == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem item)
+        {
+            if(item == null)
+            {
+                return BadRequest();
+            }
+            ToDoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] ToDoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = ToDoItems.Find(id);
+            if(todo == null)
+            {
+                return NotFound();
+            }
+
+            ToDoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Update([FromBody] ToDoItem item, string id)
+        {
+            if(item == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = ToDoItems.Find(id);
+            if(todo == null)
+            {
+                return NotFound();
+            }
+
+            item.Key = todo.Key;
+
+            ToDoItems.Update(item);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ WebApplication2/src/WebApplication2/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace ToDoApi
+//{
+//    public class DatabaseContext : DbContext
+//    {
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            optionsBuilder.UseSqlServer(@"DSN=GV1DWH01");
+//        }
+//    }
+//}
